943ee3a513cdceae5be47e57d725bba915dd4787
Ajay Vasu <ajay_vasu@outlook.com>
2024-09-04 22:39:25 +0530

    Amazon page retrieval

--- policy_check.py
+++ policy_check.py
@@ -3,6 +3,13 @@ import requests
 
 # Retrieve robots.txt
 
-response = requests.get('https://github.com/robots.txt')
+user_agent_policy = requests.get('https://www.amazon.in/robots.txt')
 
-print(response.headers)
+robots_txt = user_agent_policy.text.splitlines()
+
+for line in robots_txt:
+    print("new line here")
+
+buy_reeses = requests.get("https://www.amazon.in/Hersheys-Reeses-Miniature-Chocolate-Sharing/dp/B07N22FPN2/")
+
+print(buy_reeses.text)
